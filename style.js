748843037c27b5b2e5c778ef761b557c2a9e0e93
Bhupesh Jain <104828846+HeyItsMeBJ@users.noreply.github.com>
2023-12-12 15:16:32 +0530

    Update style.js

--- style.js
+++ style.js
@@ -29,3 +29,33 @@ pics.forEach(function (pic1) {
     }
 })
 
+
+pics.forEach(function (pic1) {
+    pic1.addEventListener('touchstart', mousedown)
+
+    function mousedown(e) {
+        var offsetx = e.clientX - pic1.offsetLeft;
+        var offsety = e.clientY - pic1.offsetTop;
+        this.style.cursor = 'pointer';
+        var tempIndex=this.style.zIndex
+        this.style.zIndex='1000';
+        pic1.addEventListener('touchmove', mousemove);
+        pic1.addEventListener('touchend', mouseup);
+        pic1.addEventListener('mouseout',mouseup);
+
+        function mousemove(ev) {
+            var x = ev.clientX - offsetx + 'px';
+            var y = ev.clientY - offsety + 'px';
+            this.style.top = y;
+            this.style.left = x;
+        }
+
+        function mouseup() {
+            pic1.removeEventListener('touchmove', mousemove)
+            this.style.removeProperty('cursor')
+            this.style.zIndex=tempIndex;
+
+        }
+    }
+})
+
